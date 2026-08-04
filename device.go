b873d2f3e751b3ea471da09f20a9a0cffa4cef16
wwqgtxx <wwqgtxx@gmail.com>
2026-08-04 21:35:20 +0800

    split DeviceStack interface

--- device.go
+++ device.go
@@ -14,15 +14,19 @@ import (
 type Device interface {
 	tun.Device
 	N.Dialer
+	DeviceStack
 	Start() error
-	DialTCP(ctx context.Context, network string, source, destination netip.AddrPort) (net.Conn, error)
-	ListenTCP(ctx context.Context, network string, local netip.AddrPort) (net.Listener, error)
-	DialUDP(ctx context.Context, network string, source, destination netip.AddrPort) (net.Conn, error)
-	ListenUDP(ctx context.Context, network string, local netip.AddrPort) (net.PacketConn, error)
 	LocalAddresses() []netip.Addr
 	Inet4Address() netip.Addr
 	Inet6Address() netip.Addr
 	// NewEndpoint() (stack.LinkEndpoint, error)
+}
+
+type DeviceStack interface {
+	DialTCP(ctx context.Context, network string, source, destination netip.AddrPort) (net.Conn, error)
+	ListenTCP(ctx context.Context, network string, local netip.AddrPort) (net.Listener, error)
+	DialUDP(ctx context.Context, network string, source, destination netip.AddrPort) (net.Conn, error)
+	ListenUDP(ctx context.Context, network string, local netip.AddrPort) (net.PacketConn, error)
 }
 
 type RegisterForward interface {
